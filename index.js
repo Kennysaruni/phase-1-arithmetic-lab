8ad7491231cb25fca541da02a59d5d059935f5b5
Kennysaruni <kenny.saruni@student.moringaschool.com>
2022-11-26 15:11:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let multiply
+let num1=31
+let num2=2
+multiply=num1*num2
+let num3=99
+let num4=5
+let mod
+mod=num3%num4
+let max
+max=Math.max(5,10,15,17,20)
+let random
+random=Math.floor(Math.random() * 10) + 1;
